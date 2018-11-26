ea643b78115746693fd9996f541ead649a8837db
Dupanov Vladislav <43404808+650501student@users.noreply.github.com>
2018-11-27 01:38:47 +0300

    Create practicewindow.h

---
+++ Source Code/practicewindow.h
@@ -0,0 +1,30 @@
+#pragma once
+#define PRACTICEWINDOW_H
+#include <QtWidgets>
+#include "resswin.h"
+class Test : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Test();
+    QStringList fileToList();       //перенос файла в массив строк
+    QString select();               //выбор рандомного слова из массива строк
+    QString beginningSelect();      //выбор слов при запуске режима практики
+    void spaceEvent();
+private:
+    int wrongLetters, rightLetters; //количество (не)правильно введённых букв
+    int rightWords, wrongWords;     //количество (не)правильно введённых слов
+    int tmrRemainingSec;            //время на ввод
+    QString j;                      //вся строка в label
+    QLineEdit *ledit;               //поле ввода
+    QLabel *tlabel;                 //label для таймера
+    QLabel *label;                  //label для слов ввода
+    QString filename;               //путь к файлу
+    QTimer *tmr;                    //таймер
+    QStringList sList;              //массив строк, в кот. переносится файл
+private slots:
+    void newResult();               //функция создания окна результата
+    void inputLetterValidation();   //функция проверки нажатия пробела
+    void updateTime();              //обновление времени каждую секунду
+};
